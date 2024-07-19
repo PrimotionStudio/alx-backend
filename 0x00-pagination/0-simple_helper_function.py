ad915b63bf79c976056a2dda5f509929a06be5e5
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-19 23:39:29 +0100

    Added docstrings to the module and the index_range function.

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """
+This module contains the function
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
+    This function returns a tuple of size
+    two containing a start index and an end index
     """
     return ((page * page_size) - page_size, page * page_size)
