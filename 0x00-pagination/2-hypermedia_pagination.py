adba5fb513a00f194fb2ffbff7e0e7b6c008a4c5
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-20 00:26:23 +0100

    "Formatted get_hyper method signature and removed trailing whitespace."

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -44,7 +44,9 @@ class Server:
         start, end = index_range(page, page_size)
         return self.__dataset[start:end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Union[int, List[List], Optional[int]]]:
+    def get_hyper(self, page: int = 1,
+                  page_size: int = 10) -> Dict[str, Union[int, List[List],
+                                                          Optional[int]]]:
         """
         This function returns a dictionary
         containing the pagination information
@@ -69,4 +71,3 @@ class Server:
             "prev_page": prev_page,
             "total_pages": pages
         }
-        
