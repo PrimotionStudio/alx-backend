8ca2a8c6ffde56766c96474fd9872f672955a631
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-23 19:36:11 +0100

    "Updated LFUCache and MRUCache classes with new methods and logic; modified shebang lines; updated pycache files."

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -10,26 +10,61 @@ class MRUCache(BaseCaching):
     This class inherits from BaseCaching and
     uses the methods of the parent class
     """
-
     def __init__(self):
         """
         This method initializes the class
         """
         super().__init__()
+        # 0 is MRU, -1 is LRU
+        self.order = []
 
     def put(self, key, item):
         """
         This method puts an item in the cache
         """
+        # print("PUT:", key)
         if key is None or item is None:
             return
-        
+        if key in self.cache_data.keys():
+            self.cache_data[key] = item
+            self.append_order(key)
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Remove the MRU
+            print(f"DISCARD: {self.get_mru()}")
+            popped = self.cache_data.pop(self.get_mru())
+            # print("Just POPPED OUT:", popped)
+        self.append_order(key)
+        self.cache_data[key] = item
+        # print("Cache is of lenght:", len(self.cache_data))
+        # print("MAX ITEMS:", BaseCaching.MAX_ITEMS)
 
     def get(self, key):
         """
         This method gets an item from the cache
         """
+        # print("GET:", key)
         if key is not None and key in self.cache_data.keys():
+            self.append_order(key)
             return self.cache_data[key]
         else:
             return None
+
+    def append_order(self, key):
+        """
+        This method appends the key to the order
+        """
+        if key in self.order:
+            self.order.remove(key)
+        else:
+            if len(self.order) == BaseCaching.MAX_ITEMS:
+                self.order.pop(-1)
+        # append the key to the beginning to show MRU
+        self.order = [key] + self.order
+        # print("New Order:", self.order)
+
+    def get_mru(self):
+        """
+        This method returns the MRU
+        """
+        return self.order[0]
