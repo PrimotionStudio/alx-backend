af5cefc9b5bf05d054f88c4512222449bfed55d7
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-30 22:30:07 +0100

    "Updated get_locale function and index route to pass locale to template"

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -26,7 +26,7 @@ def get_locale():
     get the locale from the user
     """
     locale = request.args.get('locale')
-    if locale and locale in app.config["LANGUAGES"]:
+    if locale in app.config["LANGUAGES"]:
         return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
@@ -36,7 +36,7 @@ def index():
     """
     display the index page
     """
-    return render_template('0-index.html')
+    return render_template('0-index.html', locale=get_locale())
 
 
 if __name__ == "__main__":
