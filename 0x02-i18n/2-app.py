86bc59ef4b945dcaae07104fa119d103c0d9c119
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-30 20:35:10 +0100

    "Added Flask app with Babel for i18n and a basic index.html template"

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,1 +1,39 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+setup a basic flask app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get the locale from the user
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index():
+    """
+    display the index page
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
