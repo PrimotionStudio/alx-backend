8b5577eb44382383454b10889d32e3c95853e1d8
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-30 21:55:44 +0100

    "Updated app.py files with Babel configuration and locale selection"

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,1 +1,43 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+setup a basic flask app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app, local_selector=get_locale)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get the locale from the user
+    """
+    locale = request.args.get('locale')
+    if locale and locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index():
+    """
+    display the index page
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
