1d1ec7efe944c10f5300a935cd95a7384bf11848
The Primotion Studio <oyedelenewton@gmail.com>
2024-07-30 20:24:25 +0100

    "Added Flask and Flask-Babel setup, configured languages and timezone, and created an index route"

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+setup a basic flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+@app.route("/")
+def index():
+    """
+    display the index page
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
