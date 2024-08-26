a76dfcfd24d328aa6d54a20bf4bc5679d40b2093
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-26 22:17:17 +0100

    "Added Redis client connection and implemented hash operations"

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,1 +1,34 @@
 #!/usr/bin/node
+import redis from "redis";
+
+const client = redis.createClient();
+
+client.on("connect", () => {
+  console.log("Redis client connected to the server");
+});
+
+client.on("error", (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+const createHash = () => {
+  client.hset("HolbertonSchools", "Portland", 50, redis.print);
+  client.hset("HolbertonSchools", "Seattle", 80, redis.print);
+  client.hset("HolbertonSchools", "New York", 20, redis.print);
+  client.hset("HolbertonSchools", "Bogota", 20, redis.print);
+  client.hset("HolbertonSchools", "Cali", 40, redis.print);
+  client.hset("HolbertonSchools", "Paris", 2, redis.print);
+};
+
+const getAllHash = () => {
+  client.hgetall("HolbertonSchools", (error, value) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log(value);
+  });
+};
+
+createHash();
+getAllHash();
