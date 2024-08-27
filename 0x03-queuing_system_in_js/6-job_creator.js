4cf729da5c11db16c56f6226bac1e22627d75386
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-27 20:10:32 +0100

    "Added job creator and processor scripts for queuing system using Kue"

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+import kue from "kue";
+
+const queue = kue.createQueue();
+const jobData = {
+  phoneNumber: "5678922039",
+  message: "lorem ipsum",
+};
+
+const job = queue.createJob("push_notification_code", jobData).save((error) => {
+  if (!error) {
+    console.log(`Notification job created: ${job.id}`);
+  } else {
+    console.error(`Failed to create job: ${err.message}`);
+  }
+});
+
+job.on("complete", () => {
+  console.log("Notification job completed");
+});
+
+job.on("failed", () => {
+  console.log("Notification job failed");
+});
