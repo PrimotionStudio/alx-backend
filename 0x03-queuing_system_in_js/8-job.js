9eaed7347c304f3f4feeb07b1f81afc9963b2c1e
The Primotion Studio <oyedelenewton@gmail.com>
2024-08-27 20:37:03 +0100

    "Refactor job creation and add unit tests for createPushNotificationsJobs function"

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -5,24 +5,25 @@ const createPushNotificationsJobs = (jobs, queue) => {
   }
 
   jobs.forEach((jobData) => {
-    const job = queue
-      .create("push_notification_code_3", jobData)
-      .save((err) => {
-        if (!err) {
-          console.log(`Notification job created: ${job.id}`);
-        }
-      });
+    const job = queue.create("push_notification_code_3", jobData);
+    job.save((err) => {
+      if (err) {
+        console.error(`Failed to create a job: ${err}`);
+      } else {
+        console.log(`Notification job created: ${job.id}`);
 
-    job.on("complete", () => {
-      console.log(`Notification job ${job.id} completed`);
-    });
+        job.on("complete", () => {
+          console.log(`Notification job ${job.id} completed`);
+        });
 
-    job.on("failed", (err) => {
-      console.log(`Notification job ${job.id} failed: ${err}`);
-    });
+        job.on("failed", (err) => {
+          console.log(`Notification job ${job.id} failed: ${err}`);
+        });
 
-    job.on("progress", (progress) => {
-      console.log(`Notification job ${job.id} ${progress}% complete`);
+        job.on("progress", (progress) => {
+          console.log(`Notification job ${job.id} ${progress}% complete`);
+        });
+      }
     });
   });
 };
